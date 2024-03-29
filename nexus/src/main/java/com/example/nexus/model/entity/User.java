5dadcf0994e9a8e218cceeb251315a2ad966931a
TotLiBatLi <todor_ds12345@abv.bg>
2024-03-29 17:44:26 +0200

    [NEX-2] Create user, profile and role models

---
+++ nexus/src/main/java/com/example/nexus/model/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.nexus.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
+}
