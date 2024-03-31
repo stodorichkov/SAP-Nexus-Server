e4b5d38a2552ca5674157ef43f092f3623ec9440
stodorichkov <steliantodorichkov03@gmail.com>
2024-03-31 13:36:42 +0300

    [NEX-4] Add basic security filter chain

---
+++ nexus/src/main/java/com/example/nexus/constant/CorsConstants.java
@@ -0,0 +1,7 @@
+package com.example.nexus.constant;
+
+public class CorsConstants {
+    public static final String MAPPING_PATTERN = "/api/**";
+    public static final String ALLOWED_ORIGINS = "http://localhost:3000";
+    public static final String ALLOWED_ALL = "*";
+}
---
+++ nexus/src/main/java/com/example/nexus/constant/RequestMappingConstants.java
@@ -0,0 +1,6 @@
+package com.example.nexus.constant;
+
+public class RequestMappingConstants {
+    public static final String AUTH = "/api/auth/**";
+    public static final String ADMIN = "/api/admin/**";
+}
