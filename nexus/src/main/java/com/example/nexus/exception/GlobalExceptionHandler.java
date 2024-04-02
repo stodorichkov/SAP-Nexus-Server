76e5792327e73b434fa0318ef623a759cd80c1f5
LuckyOx04 <ivailon27@gmail.com>
2024-04-02 18:17:26 +0300

    Combined UnauthorizedException and AuthenticationException handlers.

--- nexus/src/main/java/com/example/nexus/exception/GlobalExceptionHandler.java
+++ nexus/src/main/java/com/example/nexus/exception/GlobalExceptionHandler.java
@@ -27,16 +27,8 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
-    @ExceptionHandler(UnauthorizedException.class)
+    @ExceptionHandler({UnauthorizedException.class, AuthenticationException.class})
     public ResponseEntity<String> handle(UnauthorizedException ex) {
-        log.error(ex.getMessage());
-        log.info(ex.getMessage(), ex);
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> handle(AuthenticationException ex) {
         log.error(ex.getMessage());
         log.info(ex.getMessage(), ex);
 
