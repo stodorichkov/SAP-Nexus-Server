c7551c6f78dc7fc9e8050e7cb4c0737f95e4717d
TotLiBatLi <todor_ds12345@abv.bg>
2024-04-13 00:58:23 +0300

    [NEX-9] Resolve comments

--- nexus/src/test/java/com/example/nexus/Service/ProfileServiceImplTests.java
+++ nexus/src/test/java/com/example/nexus/Service/ProfileServiceImplTests.java
@@ -4,46 +4,46 @@ import com.example.nexus.model.entity.Profile;
 import com.example.nexus.model.payload.request.AddMoneyRequest;
 import com.example.nexus.repository.ProfileRepository;
 import jakarta.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProfileServiceImplTests {
-
-    @InjectMocks
-    private ProfileServiceImpl profileService;
+    private static Profile profile;
+    private static AddMoneyRequest addMoneyRequest;
 
     @Mock
     private JwtService jwtService;
-
     @Mock
     private ProfileRepository profileRepository;
+    @InjectMocks
+    private ProfileServiceImpl profileService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @BeforeAll
+    static void setUp() {
+        profile = new Profile();
+        profile.setBalance(100.0F);
     }
 
     @Test
-    void testAddMoney() {
-
+    void addMoney() {
         HttpServletRequest request = mock(HttpServletRequest.class);
         when(jwtService.getTokenFromRequest(request)).thenReturn("dummyToken");
         when(jwtService.getUsernameFromToken("dummyToken")).thenReturn("testUser");
-
-        Profile profile = new Profile();
-        profile.setBalance(100.0F);
         when(profileRepository.findByUserUsername("testUser")).thenReturn(java.util.Optional.of(profile));
 
-        AddMoneyRequest addMoneyRequest = new AddMoneyRequest(50.0F);
+        addMoneyRequest = new AddMoneyRequest(50.0F);
 
         profileService.addMoney(addMoneyRequest, request);
 
         verify(profileRepository, times(1)).save(profile);
-        assert profile.getBalance() == 150.0F;
+
+        assertEquals(150.0F, profile.getBalance());
     }
 }
