ef56aff9520667fd892e5d5ee20490f0377fd044
stodorichkov <steliantodorichkov03@gmail.com>
2024-04-12 20:56:47 +0300

    [NEX-17] Start campaign plus unit tests

--- nexus/src/main/java/com/example/nexus/service/CampaignServiceImpl.java
+++ nexus/src/main/java/com/example/nexus/service/CampaignServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.nexus.service;
 
 import com.example.nexus.constant.MessageConstants;
 import com.example.nexus.exception.NotFoundException;
+import com.example.nexus.model.payload.request.StartCampaignRequest;
 import com.example.nexus.repository.CampaignRepository;
 import com.example.nexus.repository.ProductRepository;
 import com.example.nexus.specification.ProductSpecifications;
@@ -17,7 +18,21 @@ public class CampaignServiceImpl implements CampaignService {
     private final ProductRepository productRepository;
 
     @Override
+    public void startCampaign(StartCampaignRequest startCampaignRequest) {
+        final var campaign = this.campaignRepository.findByName(startCampaignRequest.name())
+                .orElseThrow(() -> new NotFoundException(MessageConstants.CAMPAIGN_NOT_FOUND));
+
+        campaign.setStartDate(startCampaignRequest.startDate());
+        campaign.setEndDate(startCampaignRequest.endDate());
+        campaign.setIsActive(true);
+
+        this.campaignRepository.save(campaign);
+    }
+
+    @Override
     public void stopCampaign(String campaignName) {
+        System.out.println(campaignName);
+
         final var campaign = this.campaignRepository.findByName(campaignName)
                 .orElseThrow(() -> new NotFoundException(MessageConstants.CAMPAIGN_NOT_FOUND));
         final var specification = ProductSpecifications.findByCampaignName(campaignName);
@@ -32,7 +47,7 @@ public class CampaignServiceImpl implements CampaignService {
         campaign.setEndDate(null);
         campaign.setIsActive(false);
 
-        productRepository.saveAll(products);
-        campaignRepository.save(campaign);
+        this.productRepository.saveAll(products);
+        this.campaignRepository.save(campaign);
     }
 }
