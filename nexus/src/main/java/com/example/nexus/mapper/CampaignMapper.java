516271c61914bb8392fd521b8ff333e104efddb2
LuckyOx04 <ivailon27@gmail.com>
2024-04-16 21:43:41 +0300

    [NEX-24] Implemented functionality for getting all campaigns.

--- nexus/src/main/java/com/example/nexus/mapper/CampaignMapper.java
+++ nexus/src/main/java/com/example/nexus/mapper/CampaignMapper.java
@@ -2,10 +2,12 @@ package com.example.nexus.mapper;
 
 import com.example.nexus.model.entity.Campaign;
 import com.example.nexus.model.payload.request.CampaignRequest;
+import com.example.nexus.model.payload.response.CampaignResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CampaignMapper {
     Campaign campaignRequestToCampaign(CampaignRequest campaignRequest);
+    CampaignResponse campaignToCampaignResponse(Campaign campaign);
 }
--- nexus/src/main/java/com/example/nexus/service/CampaignService.java
+++ nexus/src/main/java/com/example/nexus/service/CampaignService.java
@@ -1,9 +1,13 @@
 package com.example.nexus.service;
 
 import com.example.nexus.model.payload.request.CampaignRequest;
+import com.example.nexus.model.payload.response.CampaignResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CampaignService {
     void startCampaign(String campaignName);
     void stopCampaign(String campaignName);
     void addCampaign(CampaignRequest campaignRequest);
+    Page<CampaignResponse> getCampaigns(Pageable pageable);
 }
