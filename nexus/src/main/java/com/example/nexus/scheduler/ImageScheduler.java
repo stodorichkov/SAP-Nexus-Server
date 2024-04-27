0cbd1dbc55189da319091ada84bee4218239167f
LuckyOx04 <ivailon27@gmail.com>
2024-04-27 15:59:11 +0300

    [NEX-27] Changed the logic now an image is being deleted.

--- nexus/src/main/java/com/example/nexus/scheduler/ImageScheduler.java
+++ nexus/src/main/java/com/example/nexus/scheduler/ImageScheduler.java
@@ -8,34 +8,36 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Component
 @RequiredArgsConstructor
 public class ImageScheduler implements InitializingBean {
     private final ProductRepository productRepository;
     private final ImageConfig imageConfig;
     private final Logger logger = LoggerFactory.getLogger(ImageScheduler.class);
 
-    @Scheduled(cron = "0 0 3 * * ?")
+    @Scheduled(cron = "5 * * * * ?")
     public void deleteImage() {
-        List<String> dbImageFileNames = this.productRepository.findAll()
+        final var dbImageFileNames = this.productRepository.findAll()
                 .stream().map(Product::getImageLink).toList();
 
-        List<String> fsImageFileNames = Arrays.stream(Objects.requireNonNull(
-                new File(this.imageConfig.getBaseUrl()).list()))
-                .map(n -> this.imageConfig.getBaseUrl() + n).toList();
+        final var fsImageFileNames = Arrays.stream(Objects.requireNonNull(
+                new File(this.imageConfig.getDir()).list())).toList();
 
-        for (String fileName : fsImageFileNames) {
-            if (!dbImageFileNames.contains(fileName)) {
-                File file = new File(fileName);
-                if (file.delete()) {
-                    this.logger.info("File deleted!");
-                } else {
-                    this.logger.warn("File deletion failed!");
-                }
+        this.logger.info(String.valueOf(dbImageFileNames));
+        this.logger.info(String.valueOf(fsImageFileNames));
+
+        for (var fileName : fsImageFileNames) {
+            if (!dbImageFileNames.contains(imageConfig.getBaseUrl() + fileName)) {
+                File file = new File(imageConfig.getDir() + '/' + fileName);
+                logger.info(file.toString());
+                file.delete();
             }
         }
     }
